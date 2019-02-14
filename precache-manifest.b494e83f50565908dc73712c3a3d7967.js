5b7493b92fa480796a983205fcf4d10854f2f76b
sarita2129 <nairsarita2129@gmail.com>
2019-02-14 12:04:56 +1100

    Updates

---
+++ precache-manifest.b494e83f50565908dc73712c3a3d7967.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "526b12876006534e0e51",
+    "url": "/intro/static/css/main.92ce776e.chunk.css"
+  },
+  {
+    "revision": "526b12876006534e0e51",
+    "url": "/intro/static/js/main.526b1287.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/intro/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "7b2d56c37534c56a3b31",
+    "url": "/intro/static/js/runtime~main.7b2d56c3.js"
+  },
+  {
+    "revision": "ab2ac24ee670b3a02f5dcd337efd8a94",
+    "url": "/intro/index.html"
+  }
+];
